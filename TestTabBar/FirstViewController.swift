d9555b2a985c78a3a9ca0a49050230282c838ecf
altafmultani16 <altafmultani16@gmail.com>
2021-01-10 17:33:32 -0800

    first commit

---
+++ TestTabBar/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TestTabBar
+//
+//  Created by helpmac on 16/11/20.
+//  Copyright © 2020 TechnoTouch Infotech. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestTabBar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TestTabBar
+//
+//  Created by helpmac on 16/11/20.
+//  Copyright © 2020 TechnoTouch Infotech. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
